4cc17d94b4f78cf40d231f43eedf83dec81a68c0
Versilis Tyson <versilistyson@gmail.com>
2022-04-30 17:19:04 -0500

    feat: add logic to validate whether title is unique

--- title.ts
+++ title.ts
@@ -1,15 +1,18 @@
 function onEditTitle(e: GoogleAppsScript.Events.SheetsOnEdit) {
-    const editRowIdx = e.range.getRow()
-    const editColIdx = e.range.getColumn()
     const title = e.value
     const titleRangeValues= getRange(NamedRange.Title).getValues()
 
-    titleRangeValues.forEach((row: string[], rowIdx: number) => {
-        row.forEach((col: string, colIdx: number) => {
-            if (editRowIdx != rowIdx && editColIdx != colIdx && title == col) {
-                e.range.setValue(e.oldValue)
-                SpreadsheetApp.getUi().alert("Problem with this name has already been added")
+    let frequency = 0
+    titleRangeValues.forEach((row: string[]) => {
+        row.forEach((col: string) => {
+            if (col == title) {
+                frequency++
             }
         })
     })
+
+    if (frequency > 1) {
+        e.range.setValue(e.oldValue)
+        SpreadsheetApp.getUi().alert("Problem with this name has already been added")
+    }
 }
